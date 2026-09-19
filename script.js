da1d341b63169e5052d1ab6b27616c95f37b69b7
ghostkingorg-lab <a.ghost.king.org.a@gmail.com>
2026-09-19 12:27:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,61 @@
-document.getElementById("btn").onclick = function() {
-    alert("Переходи в телеграмм! Поспеши!");
-};
-document.getElementById("btn2").onclick = function() {
-    alert("Если кто-то сможет поддержать автора в телеграмм, любым подарком, знайте, что благодаря вам улучшается качество любого проэкта!");
-};
+// ==========================================
+// ПОЛУЧАЕМ ЭЛЕМЕНТЫ
+// ==========================================
+
+const btn1 = document.getElementById("btn1");
+const btn2 = document.getElementById("btn2");
+
+const home = document.getElementById("home");
+const stories = document.getElementById("stories");
+
+const closeBtn = document.getElementById("closeBtn");
+
+
+// ==========================================
+// ОТКРЫТЬ СПИСОК ИСТОРИЙ
+// ==========================================
+
+btn1.addEventListener("click", function () {
+
+    home.style.display = "none";
+
+    stories.style.display = "block";
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
+
+
+// ==========================================
+// ВЕРНУТЬСЯ НА ГЛАВНУЮ
+// ==========================================
+
+closeBtn.addEventListener("click", function () {
+
+    stories.style.display = "none";
+
+    home.style.display = "block";
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
+
+
+// ==========================================
+// TELEGRAM
+// ==========================================
+
+btn2.addEventListener("click", function () {
+
+    window.open(
+        "https://t.me/ghostking_h_story",
+        "_blank"
+    );
+
+});
